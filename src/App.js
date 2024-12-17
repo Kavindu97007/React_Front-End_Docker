0d9c09b782baa6c816a521b17ab83b9e7d6836b2
Kavindu97007 <gkpathirana7@gmail.com>
2024-12-17 20:54:54 +0530

    first commit-count react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>React Counter</h1>
+      <p>Current Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
